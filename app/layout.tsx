b1fa6a66b96ef49d7848fc2f3d9279e6d9f7c0f6
Hampus1990 <101344732+Hampus1990@users.noreply.github.com>
2026-06-21 22:05:19 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-export const metadata = {
-  title: "Hampus & Kelsi",
-  description: "Bröllop 22 augusti 2026"
-};
-
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="sv">
+    <html>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
