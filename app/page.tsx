41cfdce90ffb03ce380cb8951303ff2b3e0b4a5a
Hampus1990 <101344732+Hampus1990@users.noreply.github.com>
2026-06-22 09:10:10 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Hampus & Kelsi</h1>
-      <p>22 augusti 2026</p>
-      <p>Mälsåkers Slott</p>
-    </main>
-  );
-}
